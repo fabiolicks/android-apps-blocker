c007252f49e4edb782e3cea2511c1180d095cc16
Fabio Licks <fabio.licks@gmail.com>
2016-06-20 22:25:06 -0300

    hide icon on launcher

--- app/src/main/java/br/com/getmo/appsblocker/MainActivity.java
+++ app/src/main/java/br/com/getmo/appsblocker/MainActivity.java
@@ -1,16 +1,13 @@
 package br.com.getmo.appsblocker;
 
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
+import android.content.ComponentName;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         LookupService.stopActionLookup();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.unlock_settings:
+                // http://stackoverflow.com/questions/15802352/how-to-show-clear-defaults-programmatically
+                // http://stackoverflow.com/questions/11171721/how-to-call-the-usb-tethering-intent-in-android-4-0-and-3-0
+                getPackageManager().clearPackagePreferredActivities( getPackageName() );
+                Toast.makeText( this, "Configurações desbloqueadas!", Toast.LENGTH_SHORT ).show();
+                return true;
+            case R.id.hide_app:
+                // http://stackoverflow.com/questions/8134884/android-how-to-programmatically-hide-launcher-icon
+                PackageManager p = getPackageManager();
+                ComponentName componentName = new ComponentName( this, MainActivity.class ); // activity which is first time open in manifiest file which is declare as <category android:name="android.intent.category.LAUNCHER" />
+                p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                return true;
+
+//            Unhide
+//            PackageManager p = getPackageManager();
+//            ComponentName componentName = new ComponentName(this, MainActivity.class);
+//            p.setComponentEnabledSetting(componentName, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
